9b8ca091463397a5328a2a044cd66187ccdef6e6
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2023-05-02 04:03:20 -0700

    Color Selection

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,93 +1,99 @@
-import { graphql } from 'gatsby';
+import { graphql } from "gatsby"
 import React, { useEffect } from "react"
-import Footer from '../components/footer';
-import Navbar from '../components/navbar';
-import ExploreForFullPage from '../components/exploreForFullPage';
-import './notion.css'
-import ExploreAndBlog from '../components/exploreAndBlog';
-import { Helmet } from 'react-helmet';
+import Footer from "../components/footer"
+import Navbar from "../components/navbar"
+import ExploreForFullPage from "../components/exploreForFullPage"
+import "./notion.css"
+import ExploreAndBlog from "../components/exploreAndBlog"
+import { Helmet } from "react-helmet"
 import Prism from "prismjs"
 import "./prism.css"
-require(`katex/dist/katex.min.css`);
+require(`katex/dist/katex.min.css`)
 
+const BlogTemplate = ({ data }) => {
+  console.log(data)
 
-const BlogTemplate = ({data}) => {
+  const { html } = data.markdownRemark
+  const { Author, Date, Title, Type, Cover_Image } =
+    data.markdownRemark.frontmatter
 
-  console.log(data);
-
-
-  const {html} = data.markdownRemark;
-  const {Author, Date, Title, Type, Cover_Image} = data.markdownRemark.frontmatter;
-  
   useEffect(() => {
     Prism.highlightAll()
   }, [])
 
+  const TitleBox = props => {
+    return (
+      <>
+        <div className="d-flex flex-sm-column flex-row">
+          <div className="sulav ">
+            <img src={props.cover} alt="Cover" />
+          </div>
 
-  const TitleBox = (props) =>{
-    return(
-      <>
-      
-      <img className = "img-fluid mt-1" style={{ height: "140px", objectFit: "cover" , width: "100%" }} src={props.cover} alt="Cover" />
-        
-      <>
-      <div className='text-center p-4  pb-0 fw-bold'>
-        <h2>{props.titlename}</h2>
-      </div>
-      
-      <div className="text-center pb-1 fw-regular fs-6">
-        {props.authorName} - {props.dateCreated} - {props.type}
+          <>
+            <div>
+              <div className="text-start py-4  pb-0 fw-bold text-sm-center">
+                <h4 className="fs-sm-1">{props.titlename}</h4>
+              </div>
+
+              <div className="text-start  fw-regular fs-10 text-sm-center">
+                <p className="p-0 m-0"><small>{props.authorName} - {props.type} - {props.dateCreated}</small></p>
+              </div>
+            </div>
+          </>
+          
+        </div>
         <hr />
-      </div>
-      </>
-
-       
-      
       </>
     )
   }
   return (
     <>
       <Helmet>
-      <title>{Title}</title>
+        <title>{Title}</title>
       </Helmet>
-      <Navbar /> 
+      <Navbar />
       <div className="container-xxl pb-2">
-        <TitleBox titlename = {Title} authorName = {Author} dateCreated = {Date} type = {Type} cover = {Cover_Image}/>
-        
-        
-        <div className = "blogPost pb-0" dangerouslySetInnerHTML={{__html: html}} />
-        <hr className="pt-2 border border-start-0 border-end-0 border-dark"/>
-        
+        <TitleBox
+          titlename={Title}
+          authorName={Author}
+          dateCreated={Date}
+          type={Type}
+          cover={Cover_Image}
+        />
+
+        <div
+          className="blogPost pb-0"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+        <hr className="pt-2 border border-start-0 border-end-0 border-dark" />
       </div>
-    
+
       <ExploreAndBlog />
-      <Footer /> 
+      <Footer />
     </>
-  );
+  )
 }
 
-export default BlogTemplate;
-
+export default BlogTemplate
 
 export const singleBlog = graphql`
-query MyBlogQuery($slug: String) {
-  markdownRemark(frontmatter: {slug: {eq: $slug}}) {
-    html
-    frontmatter {
-      Author
-      Date
-      Fun_Meter
-      Genera
-      Status
-      Tag
-      slug
-      Title
-      Type
-      Cover_Image
+  query MyBlogQuery($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        Author
+        Date
+        Fun_Meter
+        Genera
+        Status
+        Tag
+        slug
+        Title
+        Type
+        Cover_Image
+      }
+      timeToRead
+      tableOfContents
     }
-    timeToRead
-    tableOfContents
   }
-}
 `
