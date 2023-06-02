17dddc676b12b89b18cd5bcb4dcd66af972b1654
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2023-06-02 11:10:56 -0700

    Update on the color agains

--- src/components/blogComponents/blogCards.js
+++ src/components/blogComponents/blogCards.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react"
 
-export default function BlogCards 
-(props) {
-
-	const link = "/blogs/" + props.slug;
+export default function BlogCards(props) {
+  const link = "/blogs/" + props.slug
   return (
-	
-		<div className="cardss rounded-4 mx-1 mt-1" key={props.key}>
-					<img  src={props.img} className="cardssImage" alt="Maya"  height="400px" width="400px" />
-					<a href={link}><h1 className='pb-5 px-3 pt-3'>{props.title}</h1></a>
-		</div>
-	
+    <div className="cardss rounded-4 mx-1 mt-1 bg-white" key={props.key}>
+      <img
+        src={props.img}
+        className="cardssImage"
+        alt="Maya"
+        height="400px"
+        width="400px"
+      />
+      <a href={link}>
+        <h1 className="pb-5 px-3 pt-3">{props.title}</h1>
+      </a>
+    </div>
   )
 }
