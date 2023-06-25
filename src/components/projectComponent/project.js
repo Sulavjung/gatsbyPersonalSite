a974587fc77d14315ae764119ce363a7674c783d
Sulavjung <79497159+Sulavjung@users.noreply.github.com>
2023-06-24 19:42:04 -0700

    Updated one more Project Card. Which Looks fantastic.

--- src/components/projectComponent/project.js
+++ src/components/projectComponent/project.js
@@ -2,6 +2,7 @@ import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import "./style.css"
 
+
 const Project = () => {
 
 
@@ -15,8 +16,20 @@ const Project = () => {
   const link = "https://registermanagement.onrender.com"
   const buttonName = "Live Demo"
 
+/* For the Tools Project Card */
+  const mainImage1 = "../../assets/realEstateAnalysis.png"
+  const logo1 = "../../assets/realEstateAnalysislogo.png"
+  const title1 = "Business Tools"
+  const type1 = "Utility"
+  const year1 = "2023"
+  const description1 =
+    "💼 Affordable and user-friendly tools for small businesses, fueling productivity, efficiency, and growth. 💡"
+  const link1 = "https://sulavtools.netlify.app"
+  const buttonName1 = "Live Demo"
+
 
   return (
+	<>
     <div className="container-xxl py-2 px-xxl-0 p-4 mt-5 mb-5 ">
       <div className="row projectCont">
         <div className="col-lg-6 order-lg-2 order-2 pt-lg-5 mt-lg-5 projectImage">
@@ -66,7 +79,57 @@ const Project = () => {
         </div>
       </div>
     </div>
+	<div className="container-xxl py-2 px-xxl-0 p-4 mt-5 mb-5 ">
+	<div className="row projectCont">
+	  <div className="col-lg-6 order-lg-2 order-2 pt-lg-5 mt-lg-5 projectImage">
+		<StaticImage
+		  className="javacheat img-fluid"
+		  src={mainImage1}
+		  alt="Register Management System"
+		/>
+	  </div>
+	  <div className="col-lg-6 order-lg-1 order-1  d-flex align-items-center projectText">
+		<div className="p-1 p-sm-5 pt-3 pt-lg-0 pb-lg-0">
+		  <div className="labelcheat text-start p-4">
+			<div className="registerLogo">
+			  <StaticImage
+				className="javacheat img-fluid"
+				src={logo1}
+				alt="Register Management System"
+			  />
+			</div>
+			<h1>
+			  <strong>{title1}</strong>
+			</h1>
+			<p className="projectSubText">
+			  <strong>{type1} &#9864; {year1}</strong>{" "}
+			</p>
+			<div className="py-1">
+			  <p>
+				{description1}
+			  </p>
+			</div>
+			<p>
+			  <button
+				onClick={() =>
+				  window.open(
+					link1,
+					"_blank"
+				  )
+				}
+			  >
+				<p className="px-4 m-0" id="download">
+				  {buttonName1}
+				</p>
+			  </button>
+			</p>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </div>
+  </>
   )
 }
 
-export default Project
+export default Project;
